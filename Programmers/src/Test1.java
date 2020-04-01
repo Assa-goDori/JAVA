9d1b885cbc548dc7ebbcb5ea0bc99922ae8bbd57
GDJ24 <GDJ24@DESKTOP-N2Q8M86>
2020-04-01 18:17:15 +0900

    Programmers

---
+++ Programmers/src/Test1.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+
+class Solution {
+    public int solution(String arrangement) {
+    	int answer = 0;
+    	int size = 0;
+    	Stack<Character> s = new Stack<Character>();
+    	for(int i = 0; i<arrangement.length();i++) {
+        	if(arrangement.charAt(i) == '(') {
+        		s.push(arrangement.charAt(i));
+        	} else if(arrangement.charAt(i) == ')') {
+        		if(arrangement.charAt(i-1) == '(') {
+        			s.pop();
+        			if(s.size()==0) continue;
+        			answer += s.size();
+        		} else {
+        			s.pop();
+        			answer++;
+        		}
+        	}
+    	}
+    	return answer;
+    }
+}
+
+public class Test1 {
+	public static void main(String[] args) {
+		System.out.println(new Solution().solution("(()())"));
+	}
+}
