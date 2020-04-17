c872be5b90d19303f77267e27a2013ce0a0a0594
GDJ24 <GDJ24@DESKTOP-N2Q8M86>
2020-04-17 17:58:31 +0900

    Programmers

--- Programmers/src/Test2.java
+++ Programmers/src/Test2.java
@@ -3,30 +3,29 @@ import java.util.*;
 public class Test2 {
 	public static void main(String[] args) {
 		String answer = "";
-        String[] participant = {"mislav", "stanko", "mislav", "ana"};
-        String[] completion = {"stanko","ana","mislav"};
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden","kiki"};
         Map<Integer,String> map = new HashMap<Integer,String>();
-        Map<Integer,String> map2 = new HashMap<Integer,String>();
-        for(int i = 0; i<participant.length; i++) 
-        	map.put(i, participant[i]);
-        for(int j = 0; j<completion.length;j++)
-        	map2.put(j, completion[j]);
-        System.out.println(map);
-        for(int i = 0; i<participant.length;i++) {
-        	int k=0;
-        	for(int j = 0; j<completion.length; j++) {
-        		if(map.get(k)==map.get(j)) {
+        for(int i = 0; i<participant.length; i++) {
+        	map.put(i,participant[i]);
+        }
+        for(int j = 0; j<completion.length;j++) {
+        	if(map.size()==1) break;
+        	for(int k = 0; k<participant.length;k++) {
+        		if(completion[j].equals(map.get(k))) {
         			map.remove(k);
-        			System.out.println(map);
+        			break;
         		}
         	}
         }
-        System.out.println(map);
-   
-        Set<Integer> keys = map.keySet();
-        List<Integer> list = new ArrayList<>(keys);
-        answer = map.get(list.get(0));
-
+        int m = 0;
+        while(true) {
+        	if(map.get(m)!=null) {
+        		answer = map.get(m);
+        		break;
+        	}
+        	m++;
+        }
         
         System.out.println("Á¤´ä:"+answer);
 	}
